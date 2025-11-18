beb5e5a47fbc66a1f24916c4154aefd3fd867ec2
Ayan <44862892+ayan8125@users.noreply.github.com>
2025-11-18 23:42:02 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,1 @@
 echo 'hi'
-containers=$(sudo docker ps -q)
-echo "$containers"
-if [ -n "$containers" ]; then
-    sudo docker rm -f $containers
-    echo "Containers removed."
-else
-    echo "No containers running."
-fi
