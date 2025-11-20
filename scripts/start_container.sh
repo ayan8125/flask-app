c5ceaade4adc3415350235541df5e4bc993011f3
Ayan <44862892+ayan8125@users.noreply.github.com>
2025-11-20 16:30:42 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
-docker pull ayanatdocker/flask-app:version1
+docker pull ayanatdocker/flask-app-version1:version1
 
-docker run -d -p 5000:5000 ayanatdocker/flask-app:version1
+docker run -d -p 5000:5000 ayanatdocker/flask-app-version1:version1
 
